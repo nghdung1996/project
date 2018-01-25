cd5706eb18f0f3be70219d7a88f9ceb8bb477dd4
DoDat <dodat@ubuntu.ubuntu-domain>
2018-01-25 16:19:51 +0700

    chap 9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,16 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by id: session[:user_id]
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by id: user_id
+    elsif cookies_id = cookies.signed[:user_id]
+      user = User.find_by id: cookies_id
+
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -12,11 +21,30 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete :user_id
     @current_user = nil
   end
 
   def params_session
     params[:session]
+  end
+
+  def remember user
+    user.remember
+    cookies_permanent.signed[:user_id] = user.id
+    cookies_permanent[:remember_token] = user.remember_token
+  end
+
+  private
+
+  def cookies_permanent
+    cookies.permanent
+  end
+
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,46 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum: Settings.name.maximum}
   validates :email, format: {with: VALID_EMAIL_REGEX},
     presence: true, uniqueness: {case_sensitive: false}, length: {maximum: Settings.email.maximum}
-  validates :password, presence: true, length: {minimum: Settings.password.minimum}
+  validates :password, presence: true, length: {minimum: Settings.password.minimum}, allow_nil: true
   validates :age, presence: true, numericality: {only_integer: true}
   before_save :emaildowncase
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 
   private
 
